88bfb5485613cc4a61a7596a7b4769280be9b281
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-06-09 04:21:26 -0300

    Using devenv for sqlx

--- devenv.nix
+++ devenv.nix
@@ -1,12 +1,23 @@
 { pkgs, ... }:
+let
+  name = "osaka";
+in
 {
   packages = with pkgs; [
     openssl
+    sqlx-cli
+    nixfmt-rfc-style
   ];
+
+  env.DATABASE_URL = "postgres:///${name}";
 
   dotenv.enable = true;
   languages.rust = {
     enable = true;
     channel = "nightly";
+  };
+  services.postgres = {
+    enable = true;
+    initialDatabases = [ { name = name; } ];
   };
 }
