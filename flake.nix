612e8c3321a531df7fd7fb20d96a690ad61bb401
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-07-06 17:53:47 -0300

    Trying to build sqlx with docker on nix

--- flake.nix
+++ flake.nix
@@ -11,10 +11,15 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { fenix, ... }@inputs:
+    { fenix, crane, ... }@inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
@@ -24,42 +29,53 @@
       perSystem =
         { pkgs, system, ... }:
         let
-          toolchain = fenix.packages.${system}.latest;
+          DATABASE_URL = "postgres://rika:rika@localhost:5432/rika";
+          toolchain = fenix.packages.${system}.complete;
+          craneLib = (crane.mkLib pkgs).overrideToolchain toolchain.toolchain;
+          buildDeps = with pkgs; [
+            pkg-config
+            openssl
+            sqlx-cli
+          ];
         in
         {
-          packages = {
-            docker = pkgs.dockerTools.buildLayeredImage {
-              name = "rika";
+          packages.default =
+            let
+              src = ./.;
+              pkg = craneLib.buildPackage {
+                src = craneLib.cleanCargoSource src;
+                buildInputs = buildDeps ++ (with pkgs; [ docker-compose ]);
+
+                DATABASE_URL = DATABASE_URL;
+
+                preBuild = ''
+                  docker-compose -f ${src + /docker-compose.yaml} up -d
+                  sqlx database create
+                  sqlx migrate --source ${src + /migrations} run
+                  docker-compose down
+                '';
+              };
+              pkgName = "rika";
+            in
+            pkgs.dockerTools.buildLayeredImage {
+              name = pkgName;
               tag = "latest";
-              config.Cmd = "${
-                (pkgs.makeRustPlatform {
-                  cargo = toolchain.cargo;
-                  rustc = toolchain.rustc;
-                }).buildRustPackage
-                  {
-                    name = "rika";
-                    src = ./.;
-                    cargoLock.lockFile = ./Cargo.lock;
-                  }
-              }/bin/rika";
+              config.Cmd = "${pkg}/bin/${pkgName}";
+              config.Expose = "5432";
             };
-          };
 
           devShells.default = pkgs.mkShell {
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl ];
+            DATABASE_URL = DATABASE_URL;
+
             packages =
-              (with pkgs; [
-                openssl
-                pkg-config
-                sqlx-cli
-                nixfmt-rfc-style
-              ])
+              (with pkgs; [ nixfmt-rfc-style ])
               ++ (with toolchain; [
-                cargo
                 clippy
-                rustc
                 rustfmt
                 rust-analyzer
-              ]);
+              ])
+              ++ buildDeps;
           };
         };
     };
