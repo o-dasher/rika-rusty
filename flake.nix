04f86848ef8da0912aa1eeb4d5f87952147064c7
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-07-06 15:22:56 -0300

    Pure devshell without devenv

--- flake.nix
+++ flake.nix
@@ -2,43 +2,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    devenv = {
-      url = "github:cachix/devenv";
-      inputs = {
-        cachix.follows = "cachix";
-        flake-compat.follows = "flake-compat";
-        nixpkgs.follows = "nixpkgs";
-        pre-commit-hooks.follows = "pre-commit-hooks";
-      };
-    };
-
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-
-    cachix = {
-      url = "github:cachix/cachix";
-      inputs = {
-        devenv.follows = "devenv";
-        flake-compat.follows = "flake-compat";
-        nixpkgs.follows = "nixpkgs";
-        pre-commit-hooks.follows = "pre-commit-hooks";
-      };
-    };
-
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs = {
-        flake-compat.follows = "flake-compat";
-        nixpkgs.follows = "nixpkgs";
-        nixpkgs-stable.follows = "nixpkgs";
-      };
     };
 
     fenix = {
@@ -48,7 +14,7 @@
   };
 
   outputs =
-    { devenv, fenix, ... }@inputs:
+    { fenix, ... }@inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
@@ -57,50 +23,43 @@
 
       perSystem =
         { pkgs, system, ... }:
+        let
+          toolchain = fenix.packages.${system}.latest;
+        in
         {
           packages = {
             docker = pkgs.dockerTools.buildLayeredImage {
               name = "rika";
               tag = "latest";
-              config.Cmd =
-                let
-                  toolchain =
-                    with fenix.packages.${system};
-                    combine [
-                      unstable.cargo
-                      unstable.rustc
-                    ];
-                in
-                "${
-                  (pkgs.makeRustPlatform {
-                    cargo = toolchain;
-                    rustc = toolchain;
-                  }).buildRustPackage
-                    {
-                      name = "rika";
-                      src = ./.;
-                      cargoLock.lockFile = ./Cargo.lock;
-                    }
-                }/bin/rika";
+              config.Cmd = "${
+                (pkgs.makeRustPlatform {
+                  cargo = toolchain.cargo;
+                  rustc = toolchain.rustc;
+                }).buildRustPackage
+                  {
+                    name = "rika";
+                    src = ./.;
+                    cargoLock.lockFile = ./Cargo.lock;
+                  }
+              }/bin/rika";
             };
           };
 
-          devShells.default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [
-              {
-                packages = with pkgs; [
-                  openssl
-                  sqlx-cli
-                  nixfmt-rfc-style
-                ];
-
-                languages.rust = {
-                  enable = true;
-                  channel = "nightly";
-                };
-              }
-            ];
+          devShells.default = pkgs.mkShell {
+            packages =
+              (with pkgs; [
+                openssl
+                pkg-config
+                sqlx-cli
+                nixfmt-rfc-style
+              ])
+              ++ (with toolchain; [
+                cargo
+                clippy
+                rustc
+                rustfmt
+                rust-analyzer
+              ]);
           };
         };
     };
