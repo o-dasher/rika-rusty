a241518b94831f8490f29bc3163f930e756e8c78
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-07-09 00:16:49 -0300

    Adds ergo braced macro helper for nestruct

--- src/i18n/en.rs
+++ src/i18n/en.rs
@@ -1,72 +1,87 @@
 use rusty18n::{r, I18NDynamicResource, I18NFallback};
 
-use super::osaka_i_18_n::{
-    booru::{
-        blacklist::{clear::Clear, remove::Remove, Blacklist},
-        Booru,
-    },
-    errors::{self, Errors},
-    fun::{coinflip::Coinflip, Fun},
-    osu::{link::Link, Osu},
-    owner::{self, Owner},
-    user::{
-        avatar::{footer::Footer, Avatar},
-        User,
-    },
-    OsakaI18N,
-};
+use super::osaka_i_18_n::OsakaI18N;
+
+type Braceable<T> = T;
+macro_rules! ergo_braced {(
+    $base:path, $T:ty {
+        $(
+            $field_name:ident
+                // either
+                $({ $($body:tt)* })?
+                // or
+                $(: $value:expr)?
+        ),* $(,)?
+    }
+    $(,)?
+) => (::paste::paste! {
+    Braceable::<$T> {
+        $(
+            $field_name
+                // either
+                $(
+                    : ergo_braced!($base::$field_name, $base::$field_name::[< $field_name:camel >] {
+                        $($body)*
+                    })
+                )? /* or */ $(
+                    : $value
+                )?
+            ,
+        )*
+    }
+})}
 
 impl I18NFallback for OsakaI18N {
     fn fallback() -> Self {
-        Self {
-            errors: Errors {
+        ergo_braced!(super::osaka_i_18_n, Self {
+            errors {
                 unexpected: r!("Heh? Something unexpected happened with my brain."),
                 user_missing_permissions: r!("You don't have the required permissions to execute this command at this level of privilege."),
                 must_be_used_on_guild: r!("This command must be used on a server!"),
-                register: errors::register::Register {
+                register {
                     no_development_guild_set: r!("Failed to register, no development guild is set!"),
                 }
             },
-            user: User {
-                avatar: Avatar {
-                    footer: Footer {
+            user {
+                avatar {
+                    footer {
                         eq: r!("Nice, yourself!"),
                         other: r!("They are the..."),
                     },
                 },
             },
-            fun: Fun {
-                coinflip: Coinflip {
+            fun {
+                coinflip {
                     show: r!("I flip a coin and it lands on..."),
                     heads: r!("Heads"),
                     tails: r!("Tails"),
                 },
             },
-            booru: Booru {
-                blacklist: Blacklist {
+            booru {
+                blacklist {
                     blacklisted: r!(|tag| "Sure mistah, blacklisting {tag}!"),
                     everything_blacklisted_already: r!(|tag| "Hey, listen! {tag} is already on the blacklist..."),
-                    remove: Remove {
+                    remove {
                         removed: r!(|tag| "Ok, then! {tag} is not blacklisted anymore."),
                         failed: r!(|tag| "Hey, it seems that {tag} is not being blacklisted here!")
                     },
-                    clear: Clear {
+                    clear {
                         cleared: r!("Nipaa! removed everything from the blacklist for yah!"),
                         failed: r!("There is nothing to remove i'm affraid.")
                     }
                 },
             },
-            owner: Owner {
-                register: owner::register::Register {
+            owner {
+                register {
                     success: r!("Registered commands successfully!")
                 },
             },
-            osu: Osu {
-                link:  Link {
+            osu {
+                link {
                     failed: r!(|u| "Failed to link your osu! profile to {u}"),
                     linked: r!(|u| "Alright linked your osu! profile to {u}"),
                 }
             }
-        }
+        })
     }
 }
