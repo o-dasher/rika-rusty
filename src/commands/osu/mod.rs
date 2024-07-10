33eb69612f3014ec23130881876bd9c54f460c8b
o-dasher <88356162+o-dasher@users.noreply.github.com>
2024-07-10 00:29:28 -0300

    Splitting osu stuff

--- src/commands/osu/mod.rs
+++ src/commands/osu/mod.rs
@@ -2,8 +2,26 @@ use crate::{
     commands::osu::{link::link, submit::submit},
     create_command_group,
 };
+use poise::ChoiceParameter;
 
 pub mod link;
 pub mod submit;
 
 create_command_group!(osu, ["link", "submit"]);
+
+#[derive(ChoiceParameter, Default, Clone, Copy)]
+#[repr(u8)]
+pub enum Mode {
+    #[default]
+    #[name = "osu"]
+    Osu = 0,
+
+    #[name = "taiko"]
+    Taiko = 1,
+
+    #[name = "catch"]
+    Catch = 2,
+
+    #[name = "mania"]
+    Mania = 3,
+}
